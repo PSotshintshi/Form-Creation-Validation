44c3dac74dbef8e65fc21f330114dc4e61509d74
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-06-13 10:37:24 +0200

    initial commit

--- fetch-data.js
+++ fetch-data.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', fetchUserData){
 }
 try{
     const response = await fetch(apiUrl);
-    const users = await([`response.jason()`]);
+    const users = await response.jason();
        // Clear any existing content before displaying new data
         dataContainer.innerHTML = '';
           // Create a <ul> element
@@ -26,3 +26,4 @@ catch(error){
 }
 
 }
+document.addEventListener('DOMContentLoaded', fetchUserData);
