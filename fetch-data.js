e2e5539e9560eefcd28c2d7c187a0f725b4a98ba
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-06-13 10:25:44 +0200

    initial commit

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', fetchUserData){
+    async function fetchUserData(){
+        const apiUrl = 'https://jsonplaceholder.typicode.com/users.';
+      const dataContainer = document.getElementById('api-data');
+}
+try{
+    const response = await fetch(apiUrl);
+    const users = await response.jason();
+       // Clear any existing content before displaying new data
+        dataContainer.innerHTML = '';
+          // Create a <ul> element
+        const userList = document.createElement('ul');
+
+        // Loop through the users and create <li> elements
+        users.forEach(user => {
+            const listItem = document.createElement('li');
+            listItem.textContent = user.name;
+            userList.appendChild(listItem);
+});
+dataContainer.appendChild(userList);
+}
+catch(error){
+   console.error('Error fetching user data:', error);  
+    dataContainer.innerHTML = '';
+     dataContainer.textContent = `Failed to load user data.`;
+}
+
+}
